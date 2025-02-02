bd77de28586bd916f2ce33e42ae106e0f5429a27
Jonas <jonashooncantey@gmail.com>
2025-02-02 12:58:46 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -8,3 +8,15 @@ if (purchaseAmount > 100) {     //Here I let an if statement say if purchaseAmou
 }
 
 console.log(`Total amount after discount: $${purchaseAmount}`);     //Used a template literal to log the string and variable
+
+//Task 2
+console.log("Task 2")
+
+let sales = [100, 140, 180, 230, 300];       //Declaring an array of sales
+let totalSales = 0;                        //Declaring a variable of totalSales, I put its value
+                                            //at zero to make it easier to incorporate it in the for loop
+for (let i=0; i < sales.length; i++) {      //For loop that lays out the following conditions, i=0
+    totalSales += sales[i];               //If i is less than sales.length then add all sales together.
+}
+
+console.log(`Total Revenue: $${totalSales}`); //Template literal using the totalSales variable.
